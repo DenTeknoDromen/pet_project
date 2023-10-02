e3e07b077ea7d05b3a4f19cb24222850851d7bc4
Tobias Jansson <tobias.janssn@gmail.com>
2023-10-02 20:06:45 +0200

    småpyssel med cli

--- activity.py
+++ activity.py
@@ -1,10 +1,46 @@
+# import datetime
+
+
 class activity:
-    def __init__(self, name, space, starttime, stoptime):
+    def __init__(self, name, space, starttime, stoptime,):
         self.name = name
         self.space = space
         self.starttime = starttime
         self.stoptime = stoptime
+        self.people = {}
 
     def __str__(self):
-        return (f"{self.name} i {self.space}\n"
-                f"klockan {self.starttime} - {self.stoptime}")
+        # if self.starttime.date == self.stoptime.date:
+        self_str = (f"{self.name} i {self.space}\n"
+                    f"{self.starttime.strftime('%b %e %Y')}\n"
+                    f"kl {self.starttime.strftime('%H:%M')} - "
+                    f"{self.stoptime.strftime('%H:%M')}\n")
+        if len(self.people) > 0:
+            for keys in self.people:
+                self_str += f"{keys}(s): \n"
+                for values in self.people[keys]:
+                    self_str += f"\t{values}\n"
+        return self_str
+
+    def add_role(self):
+        while True:
+            indata = input("Enter name of new role: ")
+            if indata not in self.people:
+                return indata
+            else:
+                print("Role already exists, enter new role: ")
+
+    def add_member(self, key):
+        while True:
+            indata = input(f"Enter name of new {key.casefold()}(s): ")
+            return indata
+
+    def set_people(self):
+        if len(self.people) == 0:
+            self.people[self.add_role()] = []
+
+        for keys in self.people.keys():
+            print(keys)
+
+        indatakey = input("Choose role to add people to: ")
+        self.people[indatakey].append(self.add_member(indatakey))
--- booker.py
+++ booker.py
@@ -3,16 +3,26 @@ import activity
 import space
 import pickle
 
+
+lst_commands = ["show_actv", "show_spaces", "add_actv", "add_space",
+                "clear_actv", "clear_space", "list_space", "list_name",
+                "show_commands", "exit"]
 lst_activities = []
 with open("savedactivities.txt", "rb") as f:
-    lst_activities = pickle.load(f)
+    try:
+        lst_activities = pickle.load(f)
+    except EOFError:
+        lst_activities = []
 
 dict_spaces = {}
 with open("savedspaces.txt", "rb") as f:
-    dict_spaces = pickle.load(f)
+    try:
+        dict_spaces = pickle.load(f)
+    except EOFError:
+        dict_spaces = {}
 
 
-def checkconflict(lst_activities, new_activity):
+def checkconflict(new_activity):
     for x in lst_activities:
         if new_activity.space.name == x.space.name:
             if (new_activity.starttime < x.stoptime and
@@ -21,19 +31,50 @@ def checkconflict(lst_activities, new_activity):
     return True
 
 
-def newbooking():
-    name = input("Enter name: ")
-    space = input("Enter room: ")
-    starttime = input("Enter starttime (xx:xx): ")
-    stoptime = input("Enter stoptime (xx:xx): ")
-    start = datetime.time(int(starttime[0:2]), int(starttime[3:5]))
-    stop = datetime.time(int(stoptime[0:2]), int(stoptime[3:5]))
+def checkvalid(msg):
+    print(msg)
+    today = datetime.datetime.now()
+    date = input("Enter date (mm/dd): ")
+    time = input("Enter time (hh:mm): ")
+    try:
+        m = int(date[0:2])
+        d = int(date[3:5])
+        h = int(time[0:2])
+        mi = int(time[3:5])
+        returndate = datetime.datetime(today.year, m, d, h, mi)
+    except ValueError:
+        returndate = checkvalid("Invalid input, please try again")
+    if returndate.date() < today.date():
+        returndate = checkvalid("Date has already passed, please try again")
 
-    new_activity = activity.activity(name, dict_spaces[space], start, stop)
-    if checkconflict(lst_activities, new_activity):
-        lst_activities.append(new_activity)
-    else:
-        print("Schedule error")
+    return returndate
+
+
+def checkspaces():
+    while True:
+        indata = input("Enter name of space: ")
+        if indata in dict_spaces:
+            return indata
+
+
+def newbooking():
+    while True:
+        name = input("Enter name: ")
+        space = checkspaces()
+        starttime = checkvalid("Enter starttime: ")
+        stoptime = checkvalid("Enter stoptime: ")
+        if starttime > stoptime:
+            print("Invalid stoptime, please try again")
+            continue
+        new_activity = activity.activity(name, dict_spaces[space],
+                                         starttime, stoptime)
+        if checkconflict(new_activity):
+            lst_activities.append(new_activity)
+        else:
+            print("Schedule error")
+            continue
+        new_activity.set_people()
+        break
     print("-" * 30)
 
 
@@ -62,29 +103,44 @@ def listnames():
             print("-" * 30)
 
 
+def show_activities():
+    print("-" * 30)
+    for x in lst_activities:
+        print(x)
+        print("-" * 30)
+
+
+def show_spaces():
+    for y in dict_spaces:
+        print(y)
+
+
+def show_commands():
+    for x in lst_commands:
+        print(x)
+
+
 while True:
     indata = input("> ")
-    if indata == "show":
-        print("-" * 30)
-        for x in lst_activities:
-            print(x)
-            print("-" * 30)
-    elif indata == "showspaces":
-        for y in dict_spaces:
-            print(y)
-    elif indata == ("add"):
+    if indata == lst_commands[0]:
+        show_activities()
+    elif indata == lst_commands[1]:
+        show_spaces()
+    elif indata == lst_commands[2]:
         newbooking()
-    elif indata == "addspace":
+    elif indata == lst_commands[3]:
         newspace()
-    elif indata == ("clear"):
+    elif indata == lst_commands[4]:
         lst_activities.clear()
-    elif indata == "clearspace":
+    elif indata == lst_commands[5]:
         dict_spaces.clear()
-    elif indata == "listspace":
+    elif indata == lst_commands[6]:
         listspace()
-    elif indata == "listname":
+    elif indata == lst_commands[7]:
         listnames()
-    elif indata == "exit":
+    elif indata == lst_commands[8]:
+        show_commands()
+    elif indata == lst_commands[9]:
         break
 
 with open("savedactivities.txt", "wb") as f:
